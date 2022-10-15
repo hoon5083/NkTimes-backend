849b309ae38dd56f244426c7408f1a51f0c3a956
Yi Kanghoon <hoon5083@gmail.com>
2022-10-16 00:20:16 +0900

    feat(backend): createBoard service 추가(세부확인 필요)

--- src/modules/boards/boards.service.ts
+++ src/modules/boards/boards.service.ts
@@ -1,4 +1,7 @@
-import { Injectable } from "@nestjs/common";
+import { ForbiddenException, Injectable } from "@nestjs/common";
+import { Board } from "src/common/entities/board.entity";
+import { User } from "src/common/entities/user.entity";
+import { UserEnum } from "src/common/enums/user.enum";
 import { DataSource } from "typeorm";
 
 @Injectable()
@@ -6,7 +9,18 @@ export class BoardsService {
   constructor(private readonly dataSource: DataSource) {}
 
   async createBoard(currentUser, createBoardDto) {
-    return "createBoard";
+    return this.dataSource.transaction(async (manager) => {
+      const user = await manager.findOne(User, {
+        where: { email: currentUser.email },
+      });
+      if (user.authority === UserEnum.PENDING) {
+        throw new ForbiddenException("Pending User");
+      }
+      createBoardDto.applicant = user;
+      const board = await manager.create(Board, createBoardDto);
+      await manager.save(board);
+      return board;
+    });
   }
 
   async getBoards(currentUser, getBoardsQuery) {
