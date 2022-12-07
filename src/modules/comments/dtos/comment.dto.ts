a4ba54368eaea2fafa227f6289e2fc9e03d393db
YiKanghoon <hoon5083@gmail.com>
2022-12-08 05:00:23 +0900

    feat(backend): 삭제된 댓글 표시 방식 변경

--- src/modules/comments/dtos/comment.dto.ts
+++ src/modules/comments/dtos/comment.dto.ts
@@ -4,14 +4,16 @@ import { Comment } from "src/common/entities/comment.entity";
 export class CommentDto {
   constructor(comment: Comment) {
     this.id = comment.id;
-    this.content = comment.content;
+    this.content = this.isDeleted ? null : comment.content;
     this.articleId = comment.article.id;
+    this.isDeleted = comment.isDeleted;
     this.createdAt = comment.createdAt;
-    this.author = new AuthorDto(comment.author);
+    this.author = this.isDeleted ? null : new AuthorDto(comment.author);
   }
   id: number;
   content: string;
   articleId: number;
+  isDeleted: boolean;
   createdAt: Date;
   author: AuthorDto;
 }
