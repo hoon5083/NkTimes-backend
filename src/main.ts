ec81e56251c90c082f5686ff7961589ae3d50493
Yikanghoon <hoon5083@gmail.com>
2023-01-02 10:18:41 +0900

    feat(backend): 보안옵션

--- src/main.ts
+++ src/main.ts
@@ -2,16 +2,19 @@ import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 import { ConfigService } from "@nestjs/config";
 import { ValidationPipe } from "@nestjs/common";
+import helmet from "helmet";
+import * as csurf from "csurf";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    origin: "http://nktimes.nktone.com",
-    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-    preflightContinue: false,
-    optionsSuccessStatus: 204,
-  });
   const appConfig = app.get(ConfigService);
+  app.use(helmet({ contentSecurityPolicy: false }));
+  app.use(csurf());
+  app.enableCors({
+    origin: appConfig.get("app.env") === "production" ? appConfig.get("app.domain") : "*",
+    credentials: true,
+    methods: ["GET", "POST", "PATCH", "DELETE"],
+  });
   app.useGlobalPipes(
     new ValidationPipe({
       transform: true,
