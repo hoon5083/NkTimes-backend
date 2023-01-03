4f970e248de004ec4a3fd97b03dc0e26814a3be6
Yikanghoon <hoon5083@gmail.com>
2023-01-03 11:53:01 +0900

    feat(devops): cd과정 수동으로 하도록 수정

--- scripts/after-deploy.sh
+++ scripts/after-deploy.sh
@@ -3,6 +3,6 @@ REPOSITORY=/home/ubuntu/build
 
 cd $REPOSITORY
 
-sudo /usr/local/bin/yarn
+# sudo /usr/local/bin/yarn
 
-sudo /usr/local/bin/pm2 start dist/main.js
+# sudo /usr/local/bin/pm2 start dist/main.js
